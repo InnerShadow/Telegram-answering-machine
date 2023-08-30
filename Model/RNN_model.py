788fe341867da56d2a388bd388a612c80fa36dd3
Nobody <mishasikolenko@mail.ru>
2023-08-30 15:00:31 +0300

    Another RNN architecture

--- Model/RNN_model.py
+++ Model/RNN_model.py
@@ -18,19 +18,40 @@ def load_RNN_model(name):
     return model
 
 
+def full_sequence_RNN_train(model, X, Y, tokenizer, batch_size, epochs, sequences_len, maxWordsCount):
+    texts = ""
+    for j in range(len(X)):
+        texts += X[j] + " - " + Y[j] + " \n "
+
+    data = tokenizer.texts_to_sequences([texts])
+    res = np.array(data[0])
+
+    n = res.shape[0] - sequences_len
+
+    X_train = np.array([res[k:k + sequences_len] for k in range(n)])
+    Y_train = to_categorical(res[sequences_len:], num_classes = maxWordsCount)
+
+    model.fit(X_train, Y_train, batch_size = batch_size, epochs = epochs)
+
+    return model
+
+
 def sequence_RNN_train(model, X, Y, tokenizer, batch_size, epochs, sequences_len, maxWordsCount):
     for i in range(epochs):
         print("epoch " + str(i) + " from " + str(epochs))
         index = 0
         for k in range(int(len(X) / batch_size)):
             texts = ""
-            for j in range(index, index + batch_size):
+            rand_shift = np.random.randint(int(-0.1 * batch_size), 0)
+            for j in range(index + rand_shift, index + batch_size):
                 if j >= len(X):
                     break
+
+                if j < 0:
+                    continue
                 
                 texts += X[j] + " - " + Y[j] + " \n "
 
-        
             data = tokenizer.texts_to_sequences([texts])
             res = np.array(data[0])
 
@@ -44,7 +65,7 @@ def sequence_RNN_train(model, X, Y, tokenizer, batch_size, epochs, sequences_len
 
             index += batch_size
 
-            model.fit(X_train, Y_train, batch_size = batch_size)
+            model.fit(X_train, Y_train, batch_size = batch_size, epochs = 5)
 
     return model
 
@@ -103,60 +124,19 @@ def random_RNN_train(model, X, Y, tokenizer, batch_size, epochs, sequences_len, 
     return model
 
 
-def RNN_word_continue(name, X, Y, tokenizer, maxWordsCount = 5000, sequences_len = 100, batch_size = 64, epochs = 50):
+def Get_RNN_word_continue(maxWordsCount = 5000, sequences_len = 100):
 
     model = Sequential()
-    model.add(Embedding(maxWordsCount, 1024, input_length = sequences_len))
-    model.add(LSTM(512, return_sequences = True))
-    model.add(GRU(256, return_sequences = True))
+    model.add(Embedding(maxWordsCount, 512, input_length = sequences_len))
+    model.add(LSTM(256, return_sequences = True))
     model.add(GRU(128, return_sequences = True))
-    model.add(GRU(64))
+    model.add(GRU(64, return_sequences = True))
+    model.add(LSTM(32))
     model.add(Dense(maxWordsCount, activation = 'softmax'))
 
     model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 
     model.summary()
 
-    model = sequence_RNN_train(model, X, Y, tokenizer, batch_size, epochs, sequences_len, maxWordsCount)
-    #model = random_sequence_RNN_train(model, X, Y, tokenizer, batch_size, epochs, sequences_len, maxWordsCount)
-    #model = random_RNN_train(model, X, Y, tokenizer, batch_size, epochs, sequences_len, maxWordsCount)
-
-    save_RNN_model(name, model)
-
     return model
 
-    
-def RNN_word_edit_QA_model(name, X, Y, tokenizer, maxWordsCount = 5000, sequences_len = 100, batch_size = 64, epochs = 50):
-
-    data = word_level_prerpocessing(X, Y, tokenizer, sequences_len)
-
-    model = Sequential()
-    model.add(Embedding(maxWordsCount, 1024, input_length = sequences_len))
-    model.add(LSTM(512, return_sequences = True))
-    model.add(GRU(256, return_sequences = True))
-    model.add(GRU(128))
-    model.add(Dense(maxWordsCount, activation = 'softmax'))
-
-    model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
-
-    model.summary()
-
-    #model.fit(X, Y, epochs = epochs, batch_size = batch_size)
-
-    for i in range(epochs):
-        for j in range(batch_size):
-            print("Batch: " + str(j + 1) + " from " + str(batch_size) + ", epochs: " + str(i + 1) + " from " + str(epochs))
-            indx = np.random.randint(0, len(data) - 1)
-            res = np.array(data[indx])
-
-            n = res.shape[0] - sequences_len
-
-            X = np.array([res[i:i + sequences_len] for i in range(n)])
-            Y = to_categorical(res[sequences_len:], num_classes = maxWordsCount)
-
-            model.fit(X, Y, batch_size)
-
-
-    save_RNN_model(name, model)
-
-    return model
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from Data_manupulation.test_selection import SaveConversationTXT, GetTrainDataBy
 
 from Data.data import GETAPI_Hash, GetAPIID, GetPhoneNumber
 
-from Model.RNN_model import load_RNN_model, RNN_word_continue, RNN_word_edit_QA_model
+from Model.RNN_model import load_RNN_model, Get_RNN_word_continue, full_sequence_RNN_train
 from Model.Tokenizer import get_Tokinazer, save_tokinazer, load_tokinazer
 from Data_manupulation.Words_level import Word_level_answer
 
@@ -33,17 +33,20 @@ async def __main__():
     name = "@Mazar_Nozol"
     maxWordsCount = 1000
     sequences_len = 25
+    batch_size = 64
+    epochs = 50
     
     try:
         tokenizer = load_tokinazer(name)
     except Exception:
         X, Y = await (GetTrainDataByName(name, client, 2000))
         tokenizer = get_Tokinazer(X, Y, maxWordsCount = maxWordsCount)
-        save_tokinazer(name, tokenizer)
+        #save_tokinazer(name, tokenizer)
 
     text = "Но пить пиво веселее Чем кодить Имхо".lower()
 
-    model = RNN_word_continue(name, X, Y, tokenizer, maxWordsCount = maxWordsCount, epochs = 25, sequences_len = sequences_len, batch_size = 32)
+    model = Get_RNN_word_continue(maxWordsCount = maxWordsCount)
+    model = full_sequence_RNN_train(model, X, Y, tokenizer, batch_size, epochs, sequences_len, maxWordsCount)
     #model = load_RNN_model(name)
     
     print("Answ: ", Word_level_answer(model, tokenizer, text, sequences_len = sequences_len))
