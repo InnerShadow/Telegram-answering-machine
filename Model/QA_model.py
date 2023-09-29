e2cada85fa0a7f681a249fb8c3046ac2cbb8a6c6
Nobody <mishasikolenko@mail.ru>
2023-09-29 16:32:05 +0300

    Code clean

--- Model/QA_model.py
+++ Model/QA_model.py
@@ -145,7 +145,6 @@ def Get_RNN_QA(maxWordsCount = 10000, latent_dim = 200, sequences_len = 20, cont
     #Concatenate attention output with decoder outputs
     decoder_combined_context = Concatenate(axis = -1)([decoder_outputs, attention_layer])
 
-    
     #Get count contest lstm dims
     context_lstm_dims = 0
     if latent_dim < 4:
